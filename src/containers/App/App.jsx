2e2ebf0c92e98cc5b06af7465109885b89c0a844
rennova <rennova.js@gmail.com>
2019-06-13 16:41:02 +0300

    React wrapper for google maps

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import MapContainer from '../Map/MapContainer';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <MapContainer />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
