aca6d10340d06adb5aac60c7f21808b0946199bd
Vasylega Ivan <milo.duru@gmail.com>
2019-06-30 00:58:01 +0300

    make State

--- src/containers/Map/MapContainer.jsx
+++ src/containers/Map/MapContainer.jsx
@@ -1,31 +1,50 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
- 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
+
 class MapContainer extends Component {
   static defaultProps = {
+    bootstrapURLKeys:"AIzaSyC7uA0vNvcOcVJz9vSeS2Isn2zYF7q5K1w",
+    markers: [{
+      lat: 59.955413,
+      lng: 30.337844,
+      text: "My Marker"
+    }],
     center: {
       lat: 59.95,
       lng: 30.33
     },
     zoom: 11
   };
+
+  constructor(props){
+    super(props);
+    
+    this.state = Object.assign({}, this.defaultProps, props);
+  }
+
+  building (props){
+    return this.state.markers.map( (marker, row) => {
+      return (
+        <div
+          key={ row }
+          className="board-marker"
+        >
+          {marker.text}%{marker.lat}%{marker.lng}
+        </div>
+      );
+    } );
+  }
  
   render() {
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyC7uA0vNvcOcVJz9vSeS2Isn2zYF7q5K1w" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          bootstrapURLKeys={{ key: this.state.bootstrapURLKeys }}
+          defaultCenter={this.state.center}
+          defaultZoom={this.state.zoom}
         >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
+          { this.building() }
         </GoogleMapReact>
       </div>
     );
